c2ab43d7d0aad328ffcdc300b951abc5ce3a4a77
Søren Søndergaard <s.sondergaard@icloud.com>
2024-01-15 14:23:47 +0100

    Initial conversion

---
+++ Class1.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace OldButGold;
+
+public static class DateTimeHumanized
+{
+    public static string Humanize(this DateTime date)
+    {
+        var timeSpan = DateTime.Now.Subtract(date);
+
+        if (timeSpan.TotalSeconds < 60)
+        {
+            return "just now";
+        }
+
+        if (timeSpan.TotalMinutes < 60)
+        {
+            return $"{timeSpan.Minutes} minutes ago";
+        }
+
+        if (timeSpan.TotalHours < 24)
+        {
+            return $"{timeSpan.Hours} hours ago";
+        }
+
+        if (timeSpan.TotalDays < 30)
+        {
+            return $"{timeSpan.Days} days ago";
+        }
+
+        if (timeSpan.TotalDays < 365)
+        {
+            var months = Math.Round(timeSpan.TotalDays / 30);
+            return $"{months} months ago";
+        }
+
+        var years = Math.Round(timeSpan.TotalDays / 365);
+        return $"{years} years ago";
+    }
+}
